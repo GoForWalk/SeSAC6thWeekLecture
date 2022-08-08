58be07cbf635b9114bd1d6e702a2bff715b8f5eb
GoForWalk <atcgsp912@gmail.com>
2022-08-08 09:30:00 +0900

    Initial Commit

---
+++ SeSAC6thWeekLecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC6thWeekLecture
+//
+//  Created by sae hun chung on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
